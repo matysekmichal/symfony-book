abc4344e807a868d02d4b90f72037e42856e021b
Michał Matysek <hello@michalmatysek.com>
2021-08-29 20:35:26 +0200

    Add user feedback related to commenting form and add symfony/notifier package.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 use Twig\Error\LoaderError;
@@ -54,7 +56,12 @@ class HomeController extends AbstractController
      * @throws Exception
      */
     #[Route('/conferences/{slug}', name: 'conferences')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(
+        Request           $request,
+        Conference        $conference,
+        CommentRepository $commentRepository,
+        NotifierInterface $notifier,
+        string            $photoDir): Response
     {
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment);
@@ -82,7 +89,13 @@ class HomeController extends AbstractController
                 'permalink' => $request->getUri(),
             ]));
 
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
+
             return $this->redirectToRoute('conferences', ['slug' => $conference->getSlug()]);
+        }
+
+        if ($form->isSubmitted()) {
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
         }
 
         $offset = max(0, $request->query->getInt('offset'));
